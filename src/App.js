5c33d7026df56fcf37805d1f407872fb3cb2ae9d
CarnunMP <carnun@hotmail.co.uk>
2019-09-20 08:26:38 +0100

    characters fetched

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./components/Header.js";
+import axios from "axios";
 
 
 export default function App() {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://rickandmortyapi.com/api/character")
+      .then(res => {
+        setCharacters(res.data.results);
+      })
+      .catch(err => {
+        debugger
+      })
+  }, [])
+
+  console.log(characters);
+
   return (
     <main>
       <Header />
